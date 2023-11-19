6b952b24c3403d3a03786fb705d6f60be46c355b
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-11-19 11:13:57 +0100

    Add the 5th article

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     loadArticle('articles/01_hello-blog.index.html');
 
-    const navLink = document.querySelectorAll('nav ul li a');
+    const navLink = document.querySelectorAll('nav a');
     navLink.forEach(function(link) {
         link.addEventListener('click', function(event) {
             event.preventDefault();
